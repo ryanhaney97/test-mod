676015a4dc87d81befc4a7e050ec4b2845e4ff1c
Ryan Haney <yoshiquest@gmail.com>
2016-06-26 16:17:03 -0500

    Bumped Version.

--- src/main/clojure/yoshiquest/test_mod/core.clj
+++ src/main/clojure/yoshiquest/test_mod/core.clj
@@ -3,7 +3,7 @@
     [forge-clj.core :refer [defmod]]))
 
 
-(defmod test-mod "0.6.0"
+(defmod test-mod "0.6.1"
         :common {:pre-init yoshiquest.test-mod.common/common-pre-init
                  :init yoshiquest.test-mod.common/common-init}
         :client {:pre-init yoshiquest.test-mod.client/client-pre-init
